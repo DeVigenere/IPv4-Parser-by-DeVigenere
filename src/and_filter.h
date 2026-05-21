f2372f9f8f0ea8a11dc2a97648ba8782ac68e395
DeVigenere <161169792+DeVigenere@users.noreply.github.com>
2026-05-21 23:52:44 +0300

    ver0.4
    added rule of zero, i guess...

--- src/and_filter.h
+++ src/and_filter.h
@@ -3,6 +3,13 @@
 class and_filter : public filter {
 	std::vector<std::unique_ptr<filter>> filters;
 public:
+	and_filter() = default;
+	and_filter(const and_filter&) = delete;
+	and_filter& operator=(const and_filter&) = delete;
+	and_filter(and_filter&&) = default;
+	and_filter& operator=(and_filter&&) = default;
+	and_filter(const and_filter&) = delete;
+	
 	void add_filter(std::unique_ptr<filter> filter) { //add filters here
 		filters.push_back(std::move(filter));
 	}
--- src/filter.h
+++ src/filter.h
@@ -2,6 +2,12 @@
 #include <string>
 class filter {
 public:
+	filter() = default;
+	filter(const filter&) = default;
+	filter& operator=(const filter&) = default;
+	filter(filter&&) = default;
+	filter& operator=(filter&&) = default;
+	
 	virtual ~filter() = default;
 	virtual bool match(const std::string& ip) const = 0;
 	virtual std::string get_type() const = 0;
